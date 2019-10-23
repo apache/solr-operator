2600f5a6896a020aa47c5b8d49de23d84862e884
swarupdonepudi <swarup.donepudi@gmail.com>
2019-10-23 15:38:16 -0700

    just ran kubebuilder create api --group solr --version v1beta1 --kind SolrPrometheusExporter

--- main.go
+++ main.go
@@ -83,6 +83,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SolrCollection")
 		os.Exit(1)
 	}
+	if err = (&controllers.SolrPrometheusExporterReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SolrPrometheusExporter"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SolrPrometheusExporter")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
