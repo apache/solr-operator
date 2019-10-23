a2f6146d228059ffe13b243fc032b72ec153125f
swarupdonepudi <swarup.donepudi@gmail.com>
2019-10-23 15:19:45 -0700

    just ran kubebuilder create api --group solr --version v1beta1 --kind SolrCollection

--- main.go
+++ main.go
@@ -76,6 +76,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SolrBackup")
 		os.Exit(1)
 	}
+	if err = (&controllers.SolrCollectionReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SolrCollection"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SolrCollection")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
