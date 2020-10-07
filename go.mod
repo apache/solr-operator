1d1435a7a6fce0c1ba1f61e9e8f89dc8f1fd40f9
Houston Putman <houston@apache.org>
2020-10-07 13:55:13 -0400

    Upgrade dependencies of the Solr Operator. (#147)
    This includes:
    
    - GoLang 1.14
    - Kubebuilder v2.3.1
    - controller-gen v0.2.5
    - k8s.io/* v0.19.0
    - controller-runtime v0.6.2
    
    Signed-off-by: Houston Putman <houston@apache.org>

--- go.mod
+++ go.mod
@@ -1,18 +1,19 @@
 module github.com/bloomberg/solr-operator
 
-go 1.13
+go 1.14
 
 require (
 	github.com/coreos/etcd-operator v0.9.3
 	github.com/docker/spdystream v0.0.0-20181023171402-6480d4af844c // indirect
 	github.com/elazarl/goproxy v0.0.0-20191011121108-aa519ddbe484 // indirect
-	github.com/go-logr/logr v0.1.0
-	github.com/onsi/gomega v1.9.0
+	github.com/go-logr/logr v0.2.1
+	github.com/go-logr/zapr v0.2.0 // indirect
+	github.com/onsi/gomega v1.10.1
 	github.com/pravega/zookeeper-operator v0.2.6
-	github.com/stretchr/testify v1.4.0
-	golang.org/x/net v0.0.0-20200320220750-118fecf932d8
-	k8s.io/api v0.17.0
-	k8s.io/apimachinery v0.17.0
-	k8s.io/client-go v0.17.0
-	sigs.k8s.io/controller-runtime v0.4.0
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381
+	k8s.io/api v0.19.0
+	k8s.io/apimachinery v0.19.0
+	k8s.io/client-go v0.19.0
+	sigs.k8s.io/controller-runtime v0.6.2
 )
--- hack/install_dependencies.sh
+++ hack/install_dependencies.sh
@@ -4,17 +4,30 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-kubebuilder_version=2.1.0
+kubebuilder_version=2.3.1
 os=$(go env GOOS)
 arch=$(go env GOARCH)
 
 # Install go modules 
-GO111MODULE=on go mod tidy 
+GO111MODULE=on go mod tidy
 
 #Install Kustomize
-GO111MODULE=on go get sigs.k8s.io/kustomize/kustomize/v3@v3.5.4
+if !(which kustomize); then
+  (cd /tmp && curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash)
+  sudo mv "/tmp/kustomize" /usr/local/bin
+  echo "Installed kustomize at /usr/local/bin/kustomize, version: $(kustomize version --short)"
+else
+  echo "Kustomize already installed at $(which kustomize), version: $(kustomize version --short)"
+fi
 
 # Install Kubebuilder
-curl -sL https://go.kubebuilder.io/dl/${kubebuilder_version}/${os}/${arch} | tar -xz -C /tmp/
-sudo mv /tmp/kubebuilder_${kubebuilder_version}_${os}_${arch} /usr/local/kubebuilder
-export PATH=$PATH:/usr/local/kubebuilder/bin
+if !(which kubebuilder && (kubebuilder version | grep ${kubebuilder_version})); then
+  curl -sL "https://go.kubebuilder.io/dl/${kubebuilder_version}/${os}/${arch}" | tar -xz -C /tmp/
+  sudo rm -rf /usr/local/kubebuilder
+  sudo mv "/tmp/kubebuilder_${kubebuilder_version}_${os}_${arch}" /usr/local/kubebuilder
+  export PATH=$PATH:/usr/local/kubebuilder/bin
+  echo "Installed kubebuilder at /usr/local/kubebuilder/bin/kubebuilder"
+  kubebuilder version
+else
+  echo "Kubebuilder already installed at $(which kubebuilder)"
+fi
