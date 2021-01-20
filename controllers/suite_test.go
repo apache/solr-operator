964f9197f43e87b43eba1f3257bb8a7d2578b6b9
Timothy Potter <thelabdude@gmail.com>
2021-01-20 12:08:25 -0700

    revert inadvertently changes to file

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -56,7 +56,6 @@ func TestMain(m *testing.M) {
 			filepath.Join("..", "config", "crd", "bases"),
 			filepath.Join("..", "example", "dependencies"),
 		},
-		AttachControlPlaneOutput: false, // set to true to get more logging from the control plane
 	}
 	solrv1beta1.AddToScheme(scheme.Scheme)
 	zkOp.AddToScheme(scheme.Scheme)
