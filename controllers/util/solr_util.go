77d27a049b2558e25280d46646e73424ab5b953c
Jason Gerlowski <gerlowskija@apache.org>
2025-01-08 06:35:50 -0500

    Add shareProcessNamespace flag to podOptions (#735)
    Boolean flag defaults to false, and is available in the CRD at
    `.spec.customSolrKubeOptions.podOptions.shareProcessNamespace`.
    
    The 'solr' helm chart exposes this with the variable:
    `podOptions.shareProcessNamespace`.
    
    Closes #716 
    
    ---------
    
    Co-authored-by: Houston Putman <houstonputman@gmail.com>

--- controllers/util/solr_util.go
+++ controllers/util/solr_util.go
@@ -82,6 +82,7 @@ var (
 // zkConnectionString: the connectionString of the ZK instance to connect to
 func GenerateStatefulSet(solrCloud *solr.SolrCloud, solrCloudStatus *solr.SolrCloudStatus, hostNameIPs map[string]string, reconcileConfigInfo map[string]string, tls *TLSCerts, security *SecurityConfig) *appsv1.StatefulSet {
 	terminationGracePeriod := int64(60)
+	shareProcessNamespace := false
 	solrPodPort := solrCloud.Spec.SolrAddressability.PodPort
 	defaultFSGroup := int64(DefaultSolrGroup)
 
@@ -122,6 +123,7 @@ func GenerateStatefulSet(solrCloud *solr.SolrCloud, solrCloudStatus *solr.SolrCl
 		if customPodOptions.TerminationGracePeriodSeconds != nil {
 			terminationGracePeriod = *customPodOptions.TerminationGracePeriodSeconds
 		}
+		shareProcessNamespace = customPodOptions.ShareProcessNamespace
 	}
 
 	// The isNotStopped readiness gate will always be used for managedUpdates
@@ -543,6 +545,7 @@ func GenerateStatefulSet(solrCloud *solr.SolrCloud, solrCloudStatus *solr.SolrCl
 
 				Spec: corev1.PodSpec{
 					TerminationGracePeriodSeconds: &terminationGracePeriod,
+					ShareProcessNamespace:         &shareProcessNamespace,
 					SecurityContext: &corev1.PodSecurityContext{
 						FSGroup: &defaultFSGroup,
 					},
