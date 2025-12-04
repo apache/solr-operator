a46c2ef16a9fc606e1227c578e7bcb7fe5af312b
Jason Gerlowski <gerlowskija@apache.org>
2025-12-04 15:27:07 -0500

    Use 9.10 in integration tests by default

--- tests/scripts/manage_e2e_tests.sh
+++ tests/scripts/manage_e2e_tests.sh
@@ -76,7 +76,7 @@ if [[ -z "${KUBERNETES_VERSION:-}" ]]; then
   KUBERNETES_VERSION="v1.26.6"
 fi
 if [[ -z "${SOLR_IMAGE:-}" ]]; then
-  SOLR_IMAGE="${SOLR_VERSION:-9.8.1}"
+  SOLR_IMAGE="${SOLR_VERSION:-9.10.0}"
 fi
 if [[ "${SOLR_IMAGE}" != *":"* ]]; then
   SOLR_IMAGE="solr:${SOLR_IMAGE}"
